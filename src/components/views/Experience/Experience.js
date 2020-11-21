637b412f466c79223e7c3ef28b1dcd78b2ddc0e1
Binay Kumar Gupta <pratik521992@gmail.com>
2020-11-21 21:36:32 +0530

    Added initial message

--- src/components/views/Experience/Experience.js
+++ src/components/views/Experience/Experience.js
@@ -23,21 +23,83 @@ export const Experience = () => {
             wishes: " Here you will be able to read all the wishes and messages from your family, friends and loved ones",
             writerName: "click on each picture to load respective messages"
         },
-        {
+        {   id : "parents",
             main: "1",
             wishes: " asdasd",
             writerName: "There u go!!"
         },
         {
-            main: "2",
+            id : "nehadi/Jiaji",
+            main: "Jiaji and Nehadi",
+            writerName: "Chutka you are the bestest sister and bestest baashi , we all love you a lot 😍😍😍😘😘😘 you are my best friend , I can share each and everything with you , I love you very much  God bless you with lots of happiness, love and good health 😘😘😘 Happiest birthday chutka 😍😘😘😘😘😘😘 Neha didi",
+            wishes: "Happy birthday dearest Juhi! You are the sweet little sister that I got as a surprise gift. You are innocent yet very sensible. May you get everything that you ever wanted. God bless you and keep smiling always. Happy birthday :) Happy Birthday 🎈 Juhi!"
+        },
+        {
+            id : "brothers",
+            main: "3",
             wishes: " asdasd",
             writerName: "There u go!!"
         },
         {
+            id : "Pinchu",
+            main: "3",
+            wishes: " Happy birthday baashi, I love you a lot , you are my best friend😍😍😍  I love talking to you and fighting with you , you are my crime partner  ",
+            writerName: "Pinchu"
+        },
+        {
+            id : "rahul/Sakshi",
+            main: "Rahul and Sakshi",
+            wishes: " Happy Birthday 🎈 Juhi! I wish you nothing but joy and happiness, today and always. You’re an incredible friend and deserve only the best life has to offer. Have a celebratory drink (or two) from me! Happy Birthday 🎉 🥳 Rahul",
+            writerName: "There u go!!"
+        },
+        {
+            id : "Sivali",
             main: "3",
             wishes: " asdasd",
             writerName: "There u go!!"
-        }
+        },
+        {
+            id : "samba/suman",
+            main: "3",
+            wishes: " asdasd",
+            writerName: "There u go!!"
+        },
+        {
+            id : "pranay",
+            main: "3",
+            wishes: " asdasd",
+            writerName: "There u go!!"
+        },
+        {
+            id : "wasim",
+            main: "3",
+            wishes: " asdasd",
+            writerName: "There u go!!"
+        },
+        {
+            id : "shivika/Nimisha",
+            main: "3",
+            wishes: " asdasd",
+            writerName: "There u go!!"
+        },
+        {
+            id : "Abishek",
+            main: "🎈🎈🎈🎈",
+            wishes: " “On your birthday we wish for you that whatever you want most in life it comes to you just the way you imagined it or better.  Sending your way a bouquet of happiness…To wish you a very happy birthday!”🎁🍰🍫🥮🎉",
+            writerName: "Abhishek"
+        },
+        {
+            id : "Family2",
+            main: "3",
+            wishes: " asdasd",
+            writerName: "There u go!!"
+        },
+        {
+            id : "me",
+            main: "3",
+            wishes: " asdasd",
+            writerName: "There u go!!"
+        },
 
     ]
 
@@ -129,7 +191,7 @@ export const Experience = () => {
                         <div className="module ">
                             <div className="gradient-right-pannel">
                             </div></div>
-                        <div className="module area1">
+                        <div className="module ">
                             <div className="envelope-container-exp">
                                 <div onClick={() => setIndex(1)} className="gradient-right-pannel">
                                     <img id="1" style={selectedIndex === 1 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }} src="/familia.png" height="550" width="550" alt="Petrie the Powderpuff" />
@@ -142,8 +204,8 @@ export const Experience = () => {
                                 <Circles />
                             </div>
                         </div>
-                        <div className="module area2">
-                            <div className="envelope-container-exp">
+                        <div className="module ">
+                            <div id = "nehadi/Jiaji" className="envelope-container-exp">
                                 <div onClick={() => setIndex(2)} className="gradient-right-pannel">
                                     <img id="1" style={selectedIndex === 2 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }} src="/familia2.jpg" height="350" width="350" alt="Petrie the Powderpuff" />
                                 </div>
@@ -154,8 +216,8 @@ export const Experience = () => {
                                 <Circles />
                             </div>
                         </div>
-                        <div className="module area1">
-                            <div className="envelope-container-exp">
+                        <div className="module ">
+                            <div id = "brothers" className="envelope-container-exp">
                                 <div onClick={() => setIndex(3)} className="gradient-right-pannel">
                                     <img id="1" style={selectedIndex === 3 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }} src="/familia3.jpg" height="350" width="350" alt="Petrie the Powderpuff" />
                                 </div>
@@ -166,8 +228,8 @@ export const Experience = () => {
                                 <Circles />
                             </div>
                         </div>
-                        <div className="module area1">
-                            <div className="envelope-container-exp">
+                        <div className="module ">
+                            <div id = "pinchu" className="envelope-container-exp">
                                 <div onClick={() => setIndex(4)} className="gradient-right-pannel">
                                     <img id="1" style={selectedIndex === 4 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }} src="/pinchu1.jpg" height="350" width="350" alt="Petrie the Powderpuff" />
                                 </div>
@@ -178,8 +240,8 @@ export const Experience = () => {
                                 <Circles />
                             </div>
                         </div>
-                        <div className="module area1">
-                            <div className="envelope-container-exp">
+                        <div className="module ">
+                            <div  id = "sakshi" className="envelope-container-exp">
                                 <div onClick={() => setIndex(5)} className="gradient-right-pannel">
                                     <img id="1" style={selectedIndex === 5 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }} src="/friend1.jpg" height="350" width="350" alt="Petrie the Powderpuff" />
                                 </div>
@@ -190,8 +252,8 @@ export const Experience = () => {
                                 <Circles />
                             </div>
                         </div>
-                        <div className="module area1">
-                            <div className="envelope-container-exp">
+                        <div className="module ">
+                            <div id = "Shivali" className="envelope-container-exp">
                                 <div onClick={() => setIndex(6)} className="gradient-right-pannel">
                                     <img id="1" style={selectedIndex === 6 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }} src="/friend2.jpg" height="350" width="350" alt="Petrie the Powderpuff" />
                                 </div>
@@ -202,10 +264,10 @@ export const Experience = () => {
                                 <Circles />
                             </div>
                         </div>
-                        <div className="module area2">
-                            <div className="envelope-container-exp">
+                        <div className="module ">
+                            <div id = "SamaSumanWasim" className="envelope-container-exp">
                                 <div onClick={() => setIndex(7)} className="gradient-right-pannel">
-                                    <img id="1" style={selectedIndex === 7 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }} src="/friend3.jpg" height="350" width="350" alt="Petrie the Powderpuff" />
+                                    <img id="1" style={selectedIndex === 7 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }}  height="350" width="350" alt="Petrie the Powderpuff" />
                                 </div>
                                 <FormControlLabel
                                     control={<Switch checked={selectedIndex === 7} onChange={() => setIndex(7)} name="checkedA" />}
@@ -214,8 +276,8 @@ export const Experience = () => {
                                 <Circles />
                             </div>
                         </div>
-                        <div className="module area2">
-                            <div className="envelope-container-exp">
+                        <div className="module ">
+                            <div id = "shivika/Nimisha" className="envelope-container-exp">
                                 <div onClick={() => setIndex(8)} className="gradient-right-pannel">
                                     <img id="1" style={selectedIndex === 8 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }} src="/friend4.png" height="350" width="350" alt="Petrie the Powderpuff" />
                                 </div>
@@ -226,20 +288,33 @@ export const Experience = () => {
                                 <Circles />
                             </div>
                         </div>
-                        <div className="module area2">
-                            <div className="envelope-container-exp">
-                                <div onClick={() => setIndex(9)} className="gradient-right-pannel">
-                                    <img id="1" style={selectedIndex === 9 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }} src="/newFamilia.jpg" height="350" width="350" alt="Petrie the Powderpuff" />
+                        
+                        <div className="module ">
+                            <div id = "Abhishek" className="envelope-container-exp">
+                                <div onClick={() => setIndex(11)} className="gradient-right-pannel">
+                                    <img id="1" style={selectedIndex === 9 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }} src="/friend3.jpg" height="350" width="350" alt="Petrie the Powderpuff" />
                                 </div>
                                 <FormControlLabel
-                                    control={<Switch checked={selectedIndex === 9} onChange={() => setIndex(9)} name="checkedA" />}
-                                    label={selectedIndex === 9 ? "Loaded" : "Click to load"}
+                                    control={<Switch checked={selectedIndex === 11} onChange={() => setIndex(11)} name="checkedA" />}
+                                    label={selectedIndex === 11 ? "Loaded" : "Click to load"}
                                 />
                                 <Circles />
                             </div>
                         </div>
-                        <div className="module area2">
-                            <div className="envelope-container-exp">
+                        <div className="module ">
+                            <div id = "newFamilia" className="envelope-container-exp">
+                                <div onClick={() => setIndex(12)} className="gradient-right-pannel">
+                                    <img id="1" style={selectedIndex === 12 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }} src="/newFamilia.jpg" height="350" width="350" alt="Petrie the Powderpuff" />
+                                </div>
+                                <FormControlLabel
+                                    control={<Switch checked={selectedIndex === 12} onChange={() => setIndex(9)} name="checkedA" />}
+                                    label={selectedIndex === 12 ? "Loaded" : "Click to load"}
+                                />
+                                <Circles />
+                            </div>
+                        </div>
+                        <div className="module ">
+                            <div id = "me" className="envelope-container-exp">
                                 <div onClick={() => setOtpToggle(true)} className="gradient-right-pannel">
                                     <img id="1" style={selectedIndex === 10 ? { boxShadow: "19px 25px 24px 5px rgb(119 224 119 / 56%)" } : { boxShadow: "19px 25px 24px 5px #888888" }} src="/me.jpg" height="350" width="350" alt="Petrie the Powderpuff" />
                                 </div>
